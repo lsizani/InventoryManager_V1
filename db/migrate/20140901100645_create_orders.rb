1cdbb93b9be88667e7bb7fe56ccb19980dfafde2
Lunga Sizani <lsizani@gmail.com>
2014-09-01 13:08:45 +0200

    Added comments to db migrate

--- db/migrate/20140901100645_create_orders.rb
+++ db/migrate/20140901100645_create_orders.rb
@@ -1,17 +1,21 @@
 class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
-
+      #Mandatory fields for reagent request
       t.string  :requested_by
       t.date    :date_requested
       t.decimal :amount
       t.string  :name
+
+      #Mandatory fields for reagent order form
       t.string  :manufacturer
       t.string  :catalog_number
       t.string  :lot_number
       t.string  :supplier
       t.string  :order_number
       t.date    :date_ordered
+
+      #Mandatory fields for reagent delivery receipt
       t.boolean :delivered
       t.date    :date_delivered
       t.timestamps
